1832a4b03145befe0a76cb6d299b0d51df604d2b
Irza Pulungan <irzaip@gmail.com>
2025-03-13 10:41:27 +0700

    concurrency

--- run_all.sh
+++ run_all.sh
@@ -5,7 +5,7 @@ redis-server &  # Menjalankan Redis di background
 sleep 3         # Tunggu 3 detik agar Redis siap
 
 echo "Starting Celery Worker..."
-celery -A worker.celery_app worker --loglevel=info &  # Menjalankan Celery di background
+celery -A worker.celery_app worker --loglevel=info --concurrency=1 &  # Menjalankan Celery di background
 sleep 5  # Tunggu agar Celery siap
 
 echo "Starting Flask API..."
